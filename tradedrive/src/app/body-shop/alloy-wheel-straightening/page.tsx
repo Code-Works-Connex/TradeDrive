7cfae19ec63a9d4e4ecf7fc3939340a885c02653
Siluni Rupasinghe <133697974+silunirupasinghe@users.noreply.github.com>
2025-07-01 09:47:55 +0530

    alloy wheel last section

--- tradedrive/src/app/body-shop/alloy-wheel-straightening/page.tsx
+++ tradedrive/src/app/body-shop/alloy-wheel-straightening/page.tsx
@@ -1,12 +1,15 @@
 import Sec1 from '@/components/AlloyWheelStraightening/Sec1'
 import Sec2 from '@/components/AlloyWheelStraightening/Sec2'
 import Sec3 from '@/components/AlloyWheelStraightening/Sec3'
+import Sec4 from '@/components/AlloyWheelStraightening/Sec4'
+
 export default function AlloyWheelStraightening() {
   return (
     <>
     <Sec1/>
     <Sec2/>
     <Sec3/>
+    <Sec4/>
     </>
     
   );
