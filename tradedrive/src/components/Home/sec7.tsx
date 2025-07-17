c2d51a1de97aa9459e7c48d596b72bef6c20b4da
RivinduTharaka <rivindu.20220728@iit.ac.lk>
2025-07-17 14:04:56 +0530

    calander

--- tradedrive/src/components/Home/sec7.tsx
+++ tradedrive/src/components/Home/sec7.tsx
@@ -447,7 +447,7 @@ export default function Sec7() {
                             cursor: 'pointer',
                           },
                           '& input[type="date"]': {
-                            colorScheme: 'dark',
+                            colorScheme: 'light',
                           },
                         }
                         : {}),
@@ -470,7 +470,7 @@ export default function Sec7() {
                 }}
               >
                 {/* Car Body Work Group */}
-                <Box
+                <Box 
                   sx={{
                     flex: 1,
                     background: "#222",
